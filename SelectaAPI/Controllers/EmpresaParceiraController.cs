e1e64d09439526ab19e2196935937e2d9a0f33e3
devCauaRaphael <dev.cauaraphael@gmail.com>
2025-08-20 21:23:12 -0300

    adicionado controller EmpresaParceira e instalado dependencia para criptografia de senha

---
+++ SelectaAPI/Controllers/EmpresaParceiraController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SelectaAPI.Controllers
+{
+    public class EmpresaParceiraController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
