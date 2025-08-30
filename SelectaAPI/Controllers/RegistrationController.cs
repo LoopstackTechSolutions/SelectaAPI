b1029e5bd5a40aa4e160f412227b12f233f48f39
devCauaRaphael <dev.cauaraphael@gmail.com>
2025-08-30 08:32:31 -0300

    adicionado controller para cadastro

---
+++ SelectaAPI/Controllers/RegistrationController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SelectaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+    }
+}
