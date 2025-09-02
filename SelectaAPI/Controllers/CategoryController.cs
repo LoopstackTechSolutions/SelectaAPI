de6185ae52d1da9d84490aa744c5f75f55c31bbf
devCauaRaphael <dev.cauaraphael@gmail.com>
2025-09-02 11:20:47 -0300

    adicionado método para listagem de todas as categorias do sistema

---
+++ SelectaAPI/Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SelectaAPI.Database;
+
+namespace SelectaAPI.Controllers
+{
+    [Route("selectaAPI/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("get-all-categories")]
+        public async Task<IActionResult> GetAllCategory()
+        {
+            try
+            {
+                var getAll = await _context.categorias.Select(c => new
+                {
+                    c.IdCategoria,
+                    c.Nome
+                }).
+                ToListAsync();
+                return Ok(getAll);
+            }
+            catch (DbUpdateException ex)
+            {
+                return StatusCode(500, $"Erro de banco: {ex.InnerException?.Message ?? ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"erro no servidor {ex.Message}");
+            }
+        }
+    }
+}
