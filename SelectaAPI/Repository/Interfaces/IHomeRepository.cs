1ebe810e31903cb408933b0ba5e2d09145484afb
devCauaRaphael <dev.cauaraphael@gmail.com>
2025-09-03 15:50:29 -0300

    adicionando contrato na interface HomeRepository

--- SelectaAPI/Repository/Interfaces/IHomeRepository.cs
+++ SelectaAPI/Repository/Interfaces/IHomeRepository.cs
@@ -2,6 +2,11 @@
 {
     public interface IHomeRepository
     {
+        Task<HomeRepository> Search(string name);
+        Task<HomeRepository> WishList(int id);
+        Task<HomeRepository> ForYou(int id);
+        Task<HomeRepository> Highlights();
+        Task<HomeRepository> Notifications(int id);
 
     }
 }
