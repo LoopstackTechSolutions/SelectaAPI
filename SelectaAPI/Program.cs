64d4725b0a8f68c5307425878bde444cf724a7b9
devCauaRaphael <dev.cauaraphael@gmail.com>
2025-10-21 17:11:08 -0300

    alterando a string de conexão do banco de dados

--- SelectaAPI/Program.cs
+++ SelectaAPI/Program.cs
@@ -39,16 +39,16 @@ builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
 builder.Services.AddScoped<IFilesUploadAWSService, FilesUploadAWSService>();
 
-
+/*
 string connectionString =
 $"Server={Environment.GetEnvironmentVariable("SERVER")};" +
 $"Database={Environment.GetEnvironmentVariable("DATABASE")};" +
 $"User={Environment.GetEnvironmentVariable("USER")};" +
 $"Password={Environment.GetEnvironmentVariable("PASSWORD")};";
-
-/*string connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION")
-                          ?? builder.Configuration.GetConnectionString("DefaultConnection");
 */
+
+string connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION")
+                          ?? builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
